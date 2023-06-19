8493085360ff1172059506262ca9344b2dd306b0
Rize <91267260+ilanebohan@users.noreply.github.com>
2023-06-19 10:00:47 +0000

    Attempt to make a bigger game

--- js/game_manager.js
+++ js/game_manager.js
@@ -166,8 +166,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 33554432 tile
+          if (merged.value === 33554432) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
